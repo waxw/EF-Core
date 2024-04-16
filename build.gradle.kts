327382066dc43f5a857375d8973f6bd4ec43993b
miyako <weilanxiao@163.com>
2024-04-16 11:30:53 +0800

    新增 ktFormat 自定义任务

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+import org.jmailen.gradle.kotlinter.tasks.FormatTask
+import java.io.FileInputStream
+import java.io.InputStreamReader
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
   alias(libs.plugins.androidApplication) apply false
@@ -13,6 +17,26 @@ buildscript {
 
 allprojects {
   apply(plugin = "org.jmailen.kotlinter")
+}
+
+tasks.register<FormatTask>("ktFormat") {
+  val files = project.properties["formatFile"] as? String
+  files?.split(",")?.filter { it.isEmpty().not() }?.let {
+    source(it)
+  }
+  (project.properties["file"] as? String)?.let {
+    val file = file(it)
+    println("file: $it")
+    if (file.exists()) {
+      InputStreamReader(FileInputStream(file)).readLines()
+        .filter { it.isEmpty().not() }
+        .map { it.replace("${rootProject.name}/", "") }
+        .let {
+          println("format: $it")
+          source(it)
+        }
+    }
+  }
 }
 
 tasks.create("setGitHooksPath") {
