6448eb292d89165162cee705dfa3a725387b7e9c
Wei LanXiao <59554769+weilanxiao2020@users.noreply.github.com>
2024-04-17 10:48:52 +0800

    添加 Viewbinding (#10)
    * 添加 IBinding
    
    * 添加 BaseActivity
    
    * 添加 BaseFragment
    
    * 添加 BaseDialogFragment
    
    ---------
    
    Co-authored-by: miyako <weilanxiao@163.com>

---
+++ core/src/main/kotlin/com/miyako/core/viewbinding/IBinding.kt
@@ -0,0 +1,41 @@
+package com.miyako.core.viewbinding
+
+import android.view.LayoutInflater
+import androidx.annotation.MainThread
+import androidx.viewbinding.ViewBinding
+import java.lang.reflect.Method
+import java.lang.reflect.ParameterizedType
+
+sealed interface IBinding<VB : ViewBinding> {
+  val binding: VB
+
+  fun IBinding<*>.inflateBinding(inflater: LayoutInflater): VB {
+    var method: Method?
+    var clazz: Class<*> = javaClass
+    while (clazz.superclass != null) {
+      method = clazz.filterBindingMethod()
+      if (method == null) {
+        clazz = clazz.superclass
+      } else {
+        @Suppress("UNCHECKED_CAST")
+        return method.invoke(null, inflater) as VB
+      }
+    }
+    error("No Binding type argument found.")
+  }
+
+  private fun Class<*>.filterBindingMethod(): Method? {
+    return (genericSuperclass as? ParameterizedType)?.actualTypeArguments
+      ?.asSequence()
+      ?.filterIsInstance<Class<*>>()
+      ?.firstOrNull { it.simpleName.endsWith("Binding") }
+      ?.getDeclaredMethod("inflate", LayoutInflater::class.java)
+      ?.also { it.isAccessible = true }
+  }
+}
+
+sealed interface IBindingFragment<VB : ViewBinding> : IBinding<VB> {
+  @MainThread
+  fun initOnce() {
+  }
+}
