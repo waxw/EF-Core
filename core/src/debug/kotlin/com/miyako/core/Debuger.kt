d003488aa47ee47b949fec02ddb8548445a4662f
miyako <weilanxiao@163.com>
2024-04-23 18:31:07 +0800

    debugLog、measureExecuteMillis & measureExecuteNano 添加 crossinline

--- core/src/debug/kotlin/com/miyako/core/Debuger.kt
+++ core/src/debug/kotlin/com/miyako/core/Debuger.kt
@@ -7,19 +7,19 @@ inline fun <T> T?.debugLog(tag: String = "miyako"): T? {
   return this
 }
 
-inline fun <T> T?.debug(action: T?.() -> Unit): T? {
+inline fun <T> T?.debug(crossinline action: T?.() -> Unit): T? {
   this?.action()
   return this
 }
 
-inline fun <R> measureExecuteMillis(tag: String, block: () -> R): R {
+inline fun <R> measureExecuteMillis(tag: String, crossinline block: () -> R): R {
   val start = System.currentTimeMillis()
   return block().apply {
     "execute $tag: ${System.currentTimeMillis() - start}ms".debugLog()
   }
 }
 
-inline fun <R> measureExecuteNano(tag: String, block: () -> R): R {
+inline fun <R> measureExecuteNano(tag: String, crossinline block: () -> R): R {
   val start = System.nanoTime()
   return block().apply {
     "execute $tag: ${System.nanoTime() - start}ns".debugLog()
--- core/src/release/kotlin/com/miyako/core/Debuger.kt
+++ core/src/release/kotlin/com/miyako/core/Debuger.kt
@@ -2,8 +2,8 @@ package com.miyako.core
 
 inline fun <T> T?.debugLog(tag: String = "miyako") = this
 
-inline fun <T> T?.debug(action: T?.() -> Unit) = this
+inline fun <T> T?.debug(crossinline action: T?.() -> Unit) = this
 
-inline fun <R> measureExecuteMillis(tag: String, block: () -> R): R = block()
+inline fun <R> measureExecuteMillis(tag: String, crossinline block: () -> R): R = block()
 
-inline fun <R> measureExecuteNano(tag: String, block: () -> R): R = block()
+inline fun <R> measureExecuteNano(tag: String, crossinline block: () -> R): R = block()
