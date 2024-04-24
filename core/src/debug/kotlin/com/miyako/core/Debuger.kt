e5a1833da511053c5868f1e53f72989a1aaee47c
miyako <weilanxiao@163.com>
2024-04-24 12:57:48 +0800

    Debuger 添加 contract

--- core/src/debug/kotlin/com/miyako/core/Debuger.kt
+++ core/src/debug/kotlin/com/miyako/core/Debuger.kt
@@ -1,6 +1,9 @@
 package com.miyako.core
 
 import android.util.Log
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
 
 inline fun <T> T?.debugLog(tag: String = "miyako"): T? {
   Log.d(tag, this?.toString() ?: "null")
@@ -12,14 +15,22 @@ inline fun <T> T?.debug(crossinline action: T?.() -> Unit): T? {
   return this
 }
 
+@OptIn(ExperimentalContracts::class)
 inline fun <R> measureExecuteMillis(tag: String, crossinline block: () -> R): R {
+  contract {
+    callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+  }
   val start = System.currentTimeMillis()
   return block().apply {
     "execute $tag: ${System.currentTimeMillis() - start}ms".debugLog()
   }
 }
 
+@OptIn(ExperimentalContracts::class)
 inline fun <R> measureExecuteNano(tag: String, crossinline block: () -> R): R {
+  contract {
+    callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+  }
   val start = System.nanoTime()
   return block().apply {
     "execute $tag: ${System.nanoTime() - start}ns".debugLog()
@@ -27,14 +38,22 @@ inline fun <R> measureExecuteNano(tag: String, crossinline block: () -> R): R {
 }
 
 
+@OptIn(ExperimentalContracts::class)
 suspend inline fun <R> measureSuspendMillis(tag: String, crossinline block: suspend () -> R): R {
+  contract {
+    callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+  }
   val start = System.currentTimeMillis()
   return block().apply {
     "suspend $tag: ${System.currentTimeMillis() - start}ms".debugLog()
   }
 }
 
+@OptIn(ExperimentalContracts::class)
 suspend inline fun <R> measureSuspendNano(tag: String, crossinline block: suspend () -> R): R {
+  contract {
+    callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+  }
   val start = System.nanoTime()
   return block().apply {
     "suspend $tag: ${System.nanoTime() - start}ns".debugLog()
