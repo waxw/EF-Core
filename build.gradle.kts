5bcf7249cb2f760c4d20634abebe73eb4647e226
miyako <weilanxiao@163.com>
2024-04-26 10:20:08 +0800

    新增 ksp 模块

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
   alias(libs.plugins.androidApplication) apply false
   alias(libs.plugins.jetbrainsKotlinAndroid) apply false
   alias(libs.plugins.androidLibrary) apply false
+  alias(libs.plugins.jetbrainsKotlinJvm) apply false
 }
 
 buildscript {
