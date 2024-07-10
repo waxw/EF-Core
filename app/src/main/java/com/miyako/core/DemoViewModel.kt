54a54e1d230d3a57d5da126c6509646f4e9c1bb4
miyako <weilanxiao@163.com>
2024-07-10 10:00:23 +0800

    添加 DemoViewModel 演示 MVI 用法

---
+++ app/src/main/java/com/miyako/core/DemoViewModel.kt
@@ -0,0 +1,61 @@
+package com.miyako.core
+
+import com.miyako.core.ksp.mvi.Action
+import com.miyako.core.ksp.mvi.DelegateDispatch
+import com.miyako.core.ksp.mvi.DispatchAction
+import com.miyako.core.ksp.mvi.Dispatcher
+import com.miyako.core.ksp.mvi.defReturn
+import com.miyako.mvi.MviViewModel
+import com.miyako.mvi.UiAction
+import com.miyako.mvi.UiEffect
+import kotlinx.coroutines.flow.MutableStateFlow
+
+@DelegateDispatch
+class DemoViewModel : MviViewModel<DemoViewModel.UiState, UiEffect, UiAction>() {
+  override val _uiState: MutableStateFlow<UiState> = MutableStateFlow(UiState.Loading)
+
+  sealed class UiState : com.miyako.mvi.UiState {
+    data object Loading : UiState()
+    data object Error : UiState()
+    data class Success(
+      val groupId: Int,
+      val list: List<String>
+    ) : UiState()
+  }
+
+  sealed class UiAction : com.miyako.mvi.UiAction {
+    data object Back : UiAction()
+    data class Group(val settings: String) : UiAction()
+    data class Item(val settings: Int) : UiAction()
+  }
+
+  init {
+    val unitReturn = defReturn<UiAction, Unit>(tag = "Unit") {
+      "def Unit".debugLog()
+    }
+    val intReturn = defReturn<UiAction, Int>(tag = "Int") {
+      "def Int".debugLog()
+      -233
+    }
+    Dispatcher.bind(this, unitReturn, intReturn)
+  }
+
+  @DispatchAction(UiAction::class)
+  fun click(action: UiAction) {
+    Dispatcher.dispatch<UiAction, Unit>(this, action)
+  }
+
+  @DispatchAction(UiAction::class, Int::class)
+  fun clickAndReturn(action: UiAction): Int {
+    return Dispatcher.dispatch<UiAction, Int>(this, action)
+  }
+
+  @Action
+  fun clickGroup(action: UiAction.Group) {
+
+  }
+
+  @Action
+  fun clickSettings(action: UiAction.Item) {
+  }
+}
